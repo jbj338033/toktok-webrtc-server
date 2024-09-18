11cc5313b7177ca66dfd8ab09f534284c852a33b
전민오 <jbj338033@gmail.com>
2024-09-18 20:53:44 +0900

    feat: cors

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://toktok-web.mcv.kr",
     methods: ["GET", "POST"],
     credentials: true
   }
