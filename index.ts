23c3014e9403fba1b2fcc162a9525274b6599492
전민오 <jbj338033@gmail.com>
2024-09-19 07:43:43 +0900

    feat: add polling to transport

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
     credentials: true,
   },
-  transports: ['websocket']
+  transports: ['websocket', 'polling']
 });
 
 app.use(cors());
